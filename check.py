926f46b2ea7c780d187ccda96a58a7a4a363830d
chenxuuu <lolicon@papapoi.com>
2020-04-13 22:32:23 +0800

    youtube检查

--- check.py
+++ check.py
@@ -1,1 +1,39 @@
-#todo
+#coding:utf-8
+import time
+import os
+import sys
+import json
+import urllib
+import urllib.request
+import re
+import config
+
+#检查youtube频道是否开启
+def youtube(channel):
+    try:
+        url = "https://www.youtube.com/channel/"+channel+"/live"
+        html = urllib.request.urlopen(url,timeout=5).read().decode('utf-8')
+        infore = re.compile(r'ytplayer.config *= *(.+?});',re.DOTALL)
+        matchObj = infore.findall(html)
+        if not matchObj:
+            return
+        infoAll = json.loads(matchObj[0])
+        info = json.loads(infoAll['args']['player_response'])
+        return {
+            'live' : info['videoDetails']['isLive'],
+            'title' : info['videoDetails']['title'],
+            'url' : "https://youtu.be/"+info['videoDetails']['videoId'],
+            'pic' : info['videoDetails']['thumbnail']['thumbnails'][len(info['videoDetails']['thumbnail']['thumbnails'])-1]['url'],
+            'channel' : info['videoDetails']['channelId']
+        }
+    except Exception as e:
+        #print('shit')
+        #print(e)
+        return {'live' : False}
+
+def all():
+    #检查youtube
+    for channel in config.youtubeList:
+        print("check",config.youtubeList[channel])
+        status = youtube(channel)
+#all()
---
+++ config.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+
+#要监控的y2b频道
+youtubeList = {
+'UCWCc8tO-uUl_7SJXIKJACMw' : 'MEA🍥', #mea
+'UCQ0UDLQCjY0rmuxCDE38FGg' : '夏色祭🏮', #祭
+'UC1opHUrw8rvnsadT-iGp7Cg' : '湊-阿库娅⚓', #aqua
+'UCrhx4PaF3uIo9mDcTxHnmIg' : 'paryi🐇', #paryi
+'UChN7P9OhRltW3w9IesC92PA' : '森永みう🍫', #miu
+'UC8NZiqKx6fsDT3AVcMiVFyA' : '犬山💙', #犬山
+'UCH0ObmokE-zUOeihkKwWySA' : '夢乃栞-Yumeno_Shiori🍄', #大姐
+'UCjCrzObDrkYN-mELiCiSPAQ' : '夢乃栞II-Yumeno_Shiori🍄II', #大姐新频道
+'UCn14Z641OthNps7vppBvZFA' : '千草はな🌼', #hana
+'UC0g1AE0DOjBYnLhkgoRWN1w' : '本间向日葵🌻', #葵
+'UCNMG8dXjgqxS94dHljP9duQ' : 'yyut🎹', #yyut
+'UCL9dLCVvHyMiqjp2RDgowqQ' : '高槻律🚺', #律
+'UCkPIfBOLoO0hVPG-tI2YeGg' : '兔鞠mari🥕', #兔鞠mari
+'UCIdEIHpS0TdkqRkHL5OkLtA' : '名取纱那🍆', #名取纱那
+'UCBAopGXGGatkiB1-qFRG9WA' : '兔纱🎀', #兔纱
+'UCZ1WJDkMNiZ_QwHnNrVf7Pw' : '饼叽🐥', #饼叽
+'UC8gSN9D-1FL0BGBQt7p8gFQ' : '森永みう🍫小号', #森永
+'UCzAxQCoeJrmYkHr0cHfD0Nw' : 'yua🔯', #yua
+'UCerH0KOGyPaC5WueExiicZQ' : '杏💣🍠', #Anzu
+'UCPf-EnX70UM7jqjKwhDmS8g' : '玛格罗那🐟', #魔王
+'UCGcD5iUDG8xiywZeeDxye-A' : '织田信姬🍡', #织田信
+'UCXTpFs_3PqI41qX2d9tL2Rw' : '紫咲诗音🌙', #诗音
+'UCt0clH12Xk1-Ej5PXKGfdPA' : '♥️♠️物述有栖♦️♣️', #♥️♠️物述有栖♦️♣️
+}
