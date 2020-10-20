df57714f48449b6db36aaed6d61479d3ce7149f0
chenxuuu <lolicon@papapoi.com>
2020-10-20 10:10:05 +0800

    YouTube监控失效问题，有空再修

--- config.py
+++ config.py
@@ -3,8 +3,6 @@
 #要监控的y2b频道
 youtubeList = {
 'UCWCc8tO-uUl_7SJXIKJACMw' : 'MEA🍥', #mea
-'UCQ0UDLQCjY0rmuxCDE38FGg' : '夏色祭🏮', #祭
-'UC1opHUrw8rvnsadT-iGp7Cg' : '湊-阿库娅⚓', #aqua
 'UCrhx4PaF3uIo9mDcTxHnmIg' : 'paryi🐇', #paryi
 'UChN7P9OhRltW3w9IesC92PA' : '森永みう🍫', #miu
 'UC8NZiqKx6fsDT3AVcMiVFyA' : '犬山💙', #犬山
@@ -17,18 +15,15 @@ youtubeList = {
 'UCZ1WJDkMNiZ_QwHnNrVf7Pw' : '饼叽🐥', #饼叽*
 'UCzAxQCoeJrmYkHr0cHfD0Nw' : 'yua🔯', #yua*
 #'UCGcD5iUDG8xiywZeeDxye-A' : '织田信姬🍡', #织田信
-'UCXTpFs_3PqI41qX2d9tL2Rw' : '紫咲诗音🌙', #诗音
 'UCt0clH12Xk1-Ej5PXKGfdPA' : '♥️♠️物述有栖♦️♣️', #♥️♠️物述有栖♦️♣️
 }
 
 #要监控的b站频道
 bilibiliList = {
-14917277 : '湊-阿库娅⚓', #夸哥
 14052636 : '夢乃栞-Yumeno_Shiori🍄', #大姐
 12235923 : 'MEA🍥', #吊人
 4895312 : 'paryi🐇', #帕里
 7962050 : '森永みう🍫', #森永
-13946381 : '夏色祭🏮', #祭
 10545 : 'A姐💽', #adogsama
 12770821 : '千草はな🌼', #hana
 3822389 : '有栖マナ🐾', #mana
@@ -38,7 +33,6 @@ bilibiliList = {
 947447 : '高槻律🚺', #律
 3657657 : '饼叽🐥', #饼叽
 7811723 : '小狐狸Soy🌸',#soy
-21132965 : '紫咲诗音🌙', #紫咲诗音
 21449083 : '♥️♠️物述有栖♦️♣️',#♥️♠️物述有栖♦️♣️
 6632844 : '伃珍公主🦋',#Hime_sun
 }
@@ -48,7 +42,6 @@ twitcastingList = {
 'kaguramea_vov' : 'MEA🍥', #吊人
 'morinaga_miu' : '森永miu🍫', #miu
 'norioo_' : '海苔男🍡', #海苔男
-'natsuiromatsuri' : '夏色祭🏮', #夏色祭
 'c:yumeno_shiori' : 'shiori大姐🍄', #p家大姐
 #'maturin_love221' : '爱小姐☂︎', #test
 're2_takatsuki' : '高槻律🚺', #律
